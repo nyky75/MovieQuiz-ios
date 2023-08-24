cc7e67438fd5adc0f7cb19448d0d56ba3801a928
«nyky75» <nyky1937@icloud.com>
2023-08-24 15:46:44 +0300

    Refactor & Bug fix

---
+++ MovieQuiz/Models/GameRecord.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct GameRecord: Codable {
+    let correct: Int
+    let total: Int
+    let date: Date
+    
+    func isBetter(_ another: GameRecord) -> Bool {
+        return correct < another.correct
+    }
+    
+    func toString() -> String {
+        return "\(correct)/\(total) (\(date.dateTimeString)"
+    }
+}
